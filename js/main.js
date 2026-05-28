f3d6bf52e13d9c7f8c56f30455f65aadfdf3f628
Khush Patel <kmpat339@gmail.com>
2026-05-27 21:29:17 -0400

    add comment about function declaration in main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ import { setFooterYear } from "./footerYear.js";
 import { initTimeLine } from "./timeline.js";
 
 setFooterYear();
+/* Curious about this. typewriteTagLine(); is called on line 6 but declared on line 8.
+I thought you had to declare functions before calling them. I look it up and it does work
+since function declarations get hoised but just threw me off reading top to bottom. 
+I would suggest its cleaner to put function call after declaration like with imports.
+ */
 initTimeLine();
 typewriteTagLine();
 
